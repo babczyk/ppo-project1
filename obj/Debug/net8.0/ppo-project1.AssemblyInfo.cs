2391b287465f50f23fdfa74c4db30920cebc88c9
henryhenged <roniynoam@gmail.com>
2025-01-16 08:10:47 +0200

    Update assembly information and model metadata after training session

--- obj/Debug/net8.0/ppo-project1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ppo-project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ppo-project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1657a920d9e37b40dc22e86e05896905034e3690")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4279b7c9e53b9a19eccb65fd2dfc3ff90d878da")]
 [assembly: System.Reflection.AssemblyProductAttribute("ppo-project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ppo-project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
